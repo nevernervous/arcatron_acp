1d71405da03e2922e4fbca3251bdff97496b4293
nevernervous <nevernervous@email.com>
2018-02-26 20:53:53 +0800

    fixed login issue.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -58,20 +58,22 @@ class LoginController extends Controller
         $customer = null;
         $email = $request->get('email');
         $user = User::where('email', '=', $email)->first();
-        if (!$user->hasRole('admin')) {
-            $customers = $user->customers()->pluck('name')->toArray();
-            if (!in_array($customer_name, $customers))
-                return redirect()->back()
-                    ->withInput($request->only($this->username(), 'customer' ,'remember'))
-                    ->withErrors(['customer' => 'Customer does not exist.']);
-            else
-                $customer = Customer::where('name', '=', $customer_name)->first();
-        }
-        
-        if ($this->attemptLogin($request)) {
-            if ($customer)
-                Cookie::queue('cf', $customer->id, 600000);
-            return $this->sendLoginResponse($request);
+        if($user) {
+            if (!$user->hasRole('admin')) {
+                $customers = $user->customers()->pluck('name')->toArray();
+                if (!in_array($customer_name, $customers))
+                    return redirect()->back()
+                        ->withInput($request->only($this->username(), 'customer' ,'remember'))
+                        ->withErrors(['customer' => 'Customer does not exist.']);
+                else
+                    $customer = Customer::where('name', '=', $customer_name)->first();
+            }
+
+            if ($this->attemptLogin($request)) {
+                if ($customer)
+                    Cookie::queue('cf', $customer->id, 600000);
+                return $this->sendLoginResponse($request);
+            }
         }
 
         // If the login attempt was unsuccessful we will increment the number of attempts
